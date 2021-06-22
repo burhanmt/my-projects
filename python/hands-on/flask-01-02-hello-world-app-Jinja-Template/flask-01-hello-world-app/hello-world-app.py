77cf6bd24e80a57ee9e29241fcdfd7858a1a647d
burhanmt <burhanmt@icloud.com>
2021-06-22 18:25:04 +0100

    the folder was updated

--- python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,21 +1,23 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+def head():
+    return "I like Flask!"
 
 @app.route('/second')
 def second():
-    return 'Bize her yer trabzon'
+    return "This is the second page"
 
 @app.route('/third/subthird')
 def third():
-    return 'This is the subpage of the thirdpage'
+    return "This is the subpage of third page"
 
-@app.route('/fourth/<string:id>')
-def fourth(id):
-    return f'ID number of this page is {id}'
-if __name__ == '__main__':
-    #app.run(debug=True)
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id of this page is {id}'
+
+if __name__=="__main__":
     app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
